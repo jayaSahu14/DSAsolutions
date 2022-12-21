1da95fec0586d948942842d7f77fe3362b348e9e
waterlilyyy <83549217+waterlilyyy@users.noreply.github.com>
2022-12-21 14:49:10 +0530

    Initial commit

---
+++ src/main/java/DataStructures/DsAsolutionsApplication.java
@@ -0,0 +1,13 @@
+package DataStructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAsolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAsolutionsApplication.class, args);
+	}
+
+}
