e2366d7ae9df8e0a0c477260db5b7edc33ec39c8
waterlilyyy <83549217+waterlilyyy@users.noreply.github.com>
2022-12-23 16:05:46 +0530

    recursion

---
+++ src/main/java/DataStructures/question/recursion/printToN.java
@@ -0,0 +1,24 @@
+package DataStructures.question.recursion;
+
+public class printToN {
+			
+		static void printToNn(int n)
+		{
+			if(n == 0)
+				return;
+			
+			printToNn(n - 1);
+
+			System.out.print(n+" ");
+
+		}
+	    public static void main(String [] args) 
+	    {
+	    	int n = 4;
+
+	    	printToNn(n);
+	        
+	    
+
+	}
+}
